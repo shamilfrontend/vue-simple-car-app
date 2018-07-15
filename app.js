7f704309650d1a9217ac3607d51325e758f734a5
Shamil <a_shoma@mail.ru>
2018-07-15 15:46:24 +0300

    сделал вывод автомобилей

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const cars1 = [
-	'lamborghini aventador',
-	'Audi R8',
-	'lada priora',
-	'shevrole camaro',
-	'bmw m3'
+const carMake = (name, model, owner, year, phone, image) => ({
+	name, model, owner, year, phone, image
+});
+
+const cars = [
+	carMake('Lamborghini', 'Aventador', 'Nurik', '2018', '+7 925 111 22 33', './images/aventador.jpg'),
+	carMake('Audi', 'R8', 'Eldar', '2018', '+7 925 222 12 45', './images/r8.jpg'),
+	carMake('lada', 'Priora', 'Maga', '2018', '+7 925 111 22 33', './images/priora.jpg'),
+	carMake('Shevrole', 'Camaro', 'Daniil', '2017', '+7 925 777 22 43', './images/camaro.jpg'),
+	carMake('Bmw', 'M3', 'Shamil', '2018', '+7 925 111 22 33', './images/m3.jpg')
 ];
 
 new Vue({
 	el: '#app',
 	data: {
-		search: '111'
+		cars: cars,
+		car: cars[0],
+		selectedCarIndex: 0,
+	},
+	methods: {
+		selectedCarIndex(index) {
+			this.car = cars[index];
+		}
 	}
 });
